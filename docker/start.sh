abfec9171fb803c73b05e57113426d8122aaa34d
BISMARK BODONON <bismarkbodonon@gmail.com>
2026-07-31 11:26:03 +0100

    Ajout du seed conditionnel

--- docker/start.sh
+++ docker/start.sh
@@ -5,4 +5,8 @@ php artisan config:clear
 php artisan migrate --force
 php artisan storage:link || true
 
+if [ "$RUN_SEED" = "true" ]; then
+  php artisan db:seed --force
+fi
+
 php artisan serve --host=0.0.0.0 --port=${PORT:-10000}
